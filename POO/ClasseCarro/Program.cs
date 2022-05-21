343e6704d3fb78acaca7ac5e49312bf0b78ccfa4
Alan Diogo Capelari <alandc@hotmail.com.br>
2022-05-20 23:18:48 -0300

    Envio da atividade inicial de orientação a objeto.

---
+++ POO/ClasseCarro/Program.cs
@@ -0,0 +1,67 @@
+﻿/*Crie uma classe Carro com os atributos: modelo, marca, cor e placa. 
+ * Crie um método para definir os atributos, 
+ * outro método para ligar o carro 
+ * e outro método para desligar o carro.*/
+
+string a, b, x, d;
+
+Carro c = new Carro();
+Console.WriteLine("Informe a marca do carro: ");
+a = Console.ReadLine();
+
+Console.WriteLine("Informe modelo do carro: ");
+b = Console.ReadLine();
+
+Console.WriteLine("Informe a cor do carro: ");
+x = Console.ReadLine();
+
+Console.WriteLine("Informe a placa do carro: ");
+d = Console.ReadLine();
+
+c.inserirCarro(a, b, x, d);
+
+Console.WriteLine($"A marca do carro é {c.marca}, o modelo do carro é {c.modelo}, a cor do carro é {c.cor}, a placa do carro é {c.placa}");
+c.ligar();
+if (c.ligado)
+    Console.WriteLine("O carro está ligado");
+else
+    Console.WriteLine("O carro está desligado");
+
+c.desligar();
+if (c.ligado)
+    Console.WriteLine("O carro está ligado");
+else
+    Console.WriteLine("O carro está desligado");
+
+
+public class Carro
+{
+    public Carro()
+    { }
+        
+    public string marca;
+    public string modelo;
+    public string cor;
+    public string placa;
+
+    public bool ligado;
+
+    public void inserirCarro(string ma, string mo, string co, string pl)
+    {
+        marca = ma;
+        modelo = mo;
+        cor = co;
+        placa = pl;
+    }
+
+    public void ligar()
+    {
+        Console.WriteLine("Ligando o carro");
+        ligado = true;
+    }
+    public void desligar()
+    {
+        Console.WriteLine("Desligando o carro");
+        ligado = false;
+    }
+}
---
+++ POO/ProjetoPOO/Program.cs
@@ -0,0 +1,37 @@
+﻿Lampada l = new Lampada();
+l.ligar();
+l.potencia = 9;
+Console.WriteLine("O atributo ligado é = "+l.ligado+ " Potência = " + l.potencia);
+
+l.desligar();
+Console.WriteLine("O atributo ligado é = " + l.ligado);
+double n;
+n = l.retornaPotencia();
+Console.WriteLine("A potência da lâmpada é " + n);
+
+public class Lampada
+{
+    public bool ligado;
+    public double potencia;
+
+    //visibilidade (public || private || protected); void = tipo de retorno, retorna nada; int retorna inteiro; double retorna double.
+    public void ligar() 
+    {
+        Console.WriteLine("A lâmpada está sendo ligada");
+        ligado = true; 
+    }
+    public void desligar() 
+    {
+        Console.WriteLine("A lâmpada está sendo desligada");
+        ligado = false;
+    }
+    public bool estaLigada ()
+    { 
+        return ligado; 
+    }
+    public double retornaPotencia()
+    {
+        return potencia;
+    }
+    public Lampada() { }
+}
